c28bbd889f4859b82e5324425c6ecabbaf952121
Thoma77s <thomassiffc3@gmail.com>
2022-01-13 16:39:35 -0500

    add card template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var locationInputEl = document.querySelector("#location-input");
-
+//var beerContainerEl = document.querySelector(".locations-list");
 var beerFormEl = document.querySelector("#beer-form");
-
+var breweryContainerEl = document.querySelector(".locations-list")
 
 var getBreweries = function(city) {
   var apiUrl = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&sort=name:desc";
@@ -10,7 +10,7 @@ var getBreweries = function(city) {
   fetch(apiUrl).then(function(response) {
     if (response.ok) {
     response.json().then(function(city) {
-      console.log(city);
+      displayBreweries(city);
     });
   }else {
     window.alert("Error: City Not Found");
@@ -22,6 +22,8 @@ var getBreweries = function(city) {
   }
 
   var formSubmitHandler = function(event) {
+    const citiesContainer = document.getElementById('cities-container');
+    citiesContainer.innerHTML = "";
     event.preventDefault();
     var city = locationInputEl.value.trim();
   
@@ -33,6 +35,35 @@ var getBreweries = function(city) {
     }
     console.log(event);
   }
+
+  var displayBreweries = function(city) {
+    // check if api returned any repos
+    if (city.length === 0) {
+      breweryContainerEl.textContent = 'No breweries found.';
+      return;
+    }  
+    createCityCards(city);
+  };
+
+  function createCityCards(cities) { 
+    const citiesContainer = document.getElementById('cities-container');
+    // Select the card-template in the DOM
+    const cardTemplate = document.getElementById('card-template');
+    cities.forEach((city) => {
+      // Create a cardTemplate element in javascript to start populating data
+      const card = document.importNode(cardTemplate.content, true);
+      card.querySelector('.card-name').textContent = city.name;
+      card.querySelector('.card-breweryType').textContent = city.brewery_type;
+      card.querySelector('.address-street').textContent = city.street;
+      card.querySelector('.address-city').textContent = city.city;
+      card.querySelector('.address-state').textContent = city.state;
+      card.querySelector('.address-postal').textContent = city.postal;
+      card.querySelector('.card-phone').textContent = city.phone;
+      card.querySelector('.card-website-url').textContent = city.website_url;
+      card.querySelector('.card-website-url').setAttribute("href", city.website_url);
+      citiesContainer.appendChild(card);
+    });
+  };
 
   beerFormEl.addEventListener("submit", formSubmitHandler);
   
