3ef327b1d8e16012e88f11afbb7a3711e0bd93c0
Corey Gettig <coreygettig7@gmail.com>
2022-01-15 13:21:26 -0500

    added age verification modal with working buttons that loads on page load

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+//if (readCookie('pursuit_age_check') === 'true') {
+//} else {
+  //   $('#poh-age-bg').show();
+//}
+//$('.close-age-popup').click(function() {
+  //   createCookie('pursuit_age_check', 'true', 1);
+    // $('.pursuit-age-bg').hide();
+//});
