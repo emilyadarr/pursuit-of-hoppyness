d60038049a31223ccf7aef57c98cd1bcfe75cdfb
Corey Gettig <coreygettig7@gmail.com>
2022-01-15 22:51:57 -0500

    added age verification modal when page loads with working buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
 
+$(document).ready(function() {
 
-//if (readCookie('pursuit_age_check') === 'true') {
-//} else {
-  //   $('#poh-age-bg').show();
-//}
-//$('.close-age-popup').click(function() {
-  //   createCookie('pursuit_age_check', 'true', 1);
-    // $('.pursuit-age-bg').hide();
-//});
+    function ageVerify() {
+        $('#ageModal').show();
+        $('html body').css('overflow','hidden');
+    };
+
+    ageVerify();
+
+    function oldEnough() {
+        $('.age-yes-btn').click(function() {
+            $('#ageModal').hide();
+        })
+    };
+
+    oldEnough();
+
+    var catUrl = "https://www.funnycatpix.com/"
+
+    function tooYoung() {
+        $('.age-no-btn').click(function() {
+            $(location).attr('href', catUrl);
+        })
+    };
+
+    tooYoung();
+
+    
+});
+
