a261d260cf125b91632f512e18d8caca69681884
Corey Gettig <coreygettig7@gmail.com>
2022-01-17 19:02:48 -0500

    working on getting cookies working so user doesn't have to verify each time they visit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,37 @@
 
 $(document).ready(function() {
 
+//     function createCookie(name, value, days) {
+//         var expires = "";
+//         if (days) {
+//              var date = new Date();
+//              date.setTime(date.getTime() + (1 * 24 * 60 * 60 * 1000));
+//              console.log(date);
+//              expires = "; expires=" + date.toUTCString();
+//         }
+//         document.cookie = name + "=" + value + expires + "; path=/"; // affects all site pages
+//    }
+   
+//     function readCookie(name) {
+//         var nameEQ = name + "=";
+//         var ca = document.cookie.split(';');
+//         for (var i = 0; i < ca.length; i++) {
+//              var c = ca[i];
+//              while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+//              if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+//              }
+//         return null;
+//    }
+
+//     if (readCookie('pursuit_age_check') === 'true') {
+//     } else {
+//         $('#ageModal').show();
+//     }
+//     $('.close-age-popup').click(function() {
+//         createCookie('pursuit_age_check', 'true', 1);
+//         $('.pursuit-age-bg').hide();
+//     });
+    
     function ageVerify() {
         $('#ageModal').show();
         $('html body').css('overflow','hidden');
@@ -25,6 +56,7 @@ $(document).ready(function() {
     };
 
     tooYoung();
+
 
     
 });
