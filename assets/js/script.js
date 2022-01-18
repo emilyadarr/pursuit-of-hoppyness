ecc3eb66aa1e1712712ff1045c22f422ba4bfcc8
Thoma77s <thomassiffc3@gmail.com>
2022-01-18 12:18:01 -0500

    add styles to cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var locationInputEl = document.querySelector("#location-input");
 //var beerContainerEl = document.querySelector(".locations-list");
 var beerFormEl = document.querySelector("#beer-form");
-var breweryContainerEl = document.querySelector(".locations-list")
+var breweryContainerEl = document.querySelector(".locations-list");
+var likeBtnEl = document.querySelector(".favorite-btn");
 
 var getBreweries = function(city) {
   var apiUrl = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&sort=name:desc";
@@ -21,6 +22,10 @@ var getBreweries = function(city) {
     alert("Unable to connect to Database");
   });
   }
+
+  var saveFavoriteBreweries = function(){
+    localStorage.setItem(this.city, this.city.name);
+  };
 
   var formSubmitHandler = function(event) {
     const citiesContainer = document.getElementById('cities-container');
@@ -46,60 +51,64 @@ var getBreweries = function(city) {
     createCityCards(city);
   };
 
-  // function createCityCards(cities) { 
-  //   const citiesContainer = document.getElementById('cities-container');
-  //   // Select the card-template in the DOM
-  //   const cardTemplate = document.getElementById('card-template');
-  //   cities.forEach((city) => {
-  //     // Create a cardTemplate element in javascript to start populating data
-  //     const card = document.importNode(cardTemplate.content, true);
-  //     card.querySelector('.card-name').textContent = city.name;
-  //     card.querySelector('.card-breweryType').textContent = city.brewery_type;
-  //     card.querySelector('.address-street').textContent = city.street;
-  //     card.querySelector('.address-city').textContent = city.city;
-  //     card.querySelector('.address-state').textContent = city.state;
-  //     card.querySelector('.address-postal').textContent = city.postal;
-  //     card.querySelector('.card-phone').textContent = city.phone;
-  //     card.querySelector('.card-website-url').textContent = city.website_url;
-  //     card.querySelector('.card-website-url').setAttribute("href", city.website_url);
-  //     card.querySelector(".card").setAttribute("href", "./location.html?brewery=" + city.name + "?location=" + city.city);
-  //     //card.querySelector(".card").classList = "card";
-  //     citiesContainer.appendChild(card);
-  //   });
-  // };
-
   function createCityCards(cities) { 
     const citiesContainer = document.getElementById('cities-container');
+    // Select the card-template in the DOM
+    const cardTemplate = document.getElementById('card-template');
     cities.forEach((city) => {
-      // Create a card element in javascript to start populating data
-      var card = document.createElement("div");
-      var cardName = document.createElement("h3");
-      var breweryType = document.createElement("p");
-      var breweryAddress = document.createElement("p");
-      var breweryPhone = document.createElement("p");
-      var websiteBtn = document.createElement("a");
-      var likeBtn = document.createElement("a");
-      var hoursBtn = document.createElement("a");
-
-      hoursBtn.setAttribute("href", "./location.html?brewery=" + city.name + "?location=" + city.city);
-      cardName.textContent = city.name;
-      breweryType.textContent = "Type: " + city.brewery_type;
-      breweryAddress.textContent = city.street + " " + city.city + ", " + city.state;
-      breweryPhone.textContent = city.phone;
-      websiteBtn.textContent = "Brewery Website";
-      websiteBtn.setAttribute("href", city.website_url);
-      likeBtn.textContent = "Favorite";
-      hoursBtn.textContent = "View Hours";
-
-      card.classList = "card";
-      cardName.classList = "card-brewery-name";
-      websiteBtn.classList = "card-btn";
-      likeBtn.classList = "card-btn";
-      hoursBtn.classList = "card-btn";
-
-      card.append(cardName, breweryType, breweryAddress, breweryPhone, websiteBtn, likeBtn, hoursBtn);
+      // Create a cardTemplate element in javascript to start populating data
+      const card = document.importNode(cardTemplate.content, true);
+      card.querySelector('.card-brewery-name').textContent = city.name;
+      card.querySelector('.card-breweryType').textContent = city.brewery_type;
+      card.querySelector('.address-street').textContent = city.street;
+      card.querySelector('.address-city').textContent = city.city;
+      card.querySelector('.address-state').textContent = city.state;
+      card.querySelector('.address-postal').textContent = city.postal;
+      card.querySelector('.card-phone').textContent = city.phone;
+      //card.querySelector('.card-website-url').textContent = "View Website";
+      card.querySelector('.card-website-url').setAttribute("href", city.website_url);
+      card.querySelector(".view-hours").setAttribute("href", "./location.html?brewery=" + city.name + "?location=" + city.city);
+      //card.querySelector(".card").classList = "card";
+      card.querySelector('.favorite-btn').addEventListener("click", saveFavoriteBreweries);
       citiesContainer.appendChild(card);
     });
   };
 
+  // function createCityCards(cities) { 
+  //   const citiesContainer = document.getElementById('cities-container');
+  //   cities.forEach((city) => {
+  //     // Create a card element in javascript to start populating data
+  //     var card = document.createElement("div");
+  //     var cardName = document.createElement("h3");
+  //     var breweryType = document.createElement("p");
+  //     var breweryAddress = document.createElement("p");
+  //     var breweryPhone = document.createElement("p");
+  //     var websiteBtn = document.createElement("a");
+  //     var likeBtn = document.createElement("a")
+  //     var hoursBtn = document.createElement("a");
+
+  //     hoursBtn.setAttribute("href", "./location.html?brewery=" + city.name + "?location=" + city.city);
+  //     cardName.textContent = city.name;
+  //     breweryType.textContent = "Type: " + city.brewery_type;
+  //     breweryAddress.textContent = city.street + " " + city.city + ", " + city.state;
+  //     breweryPhone.textContent = city.phone;
+  //     websiteBtn.textContent = "Brewery Website";
+  //     websiteBtn.setAttribute("href", city.website_url);
+  //     likeBtn.textContent = "Favorite";
+  //     hoursBtn.textContent = "View Hours";
+
+  //     card.classList = "card";
+  //     cardName.classList = "card-brewery-name";
+  //     websiteBtn.classList = "card-btn";
+  //     likeBtn.classList = "card-btn", "favorite-btn";
+  //     hoursBtn.classList = "card-btn";
+
+  //     card.append(cardName, breweryType, breweryAddress, breweryPhone, websiteBtn, likeBtn, hoursBtn);
+  //     citiesContainer.appendChild(card);
+  //   });
+  // };
+
   beerFormEl.addEventListener("submit", formSubmitHandler);
+  
+  
+ 
